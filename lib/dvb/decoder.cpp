0433f192f0d814f4fddce9217852d7878da1130d
BarviX6 <barvix6@gmail.com>
2020-12-10 23:27:08 +0200

    spark patch

--- lib/dvb/decoder.cpp
+++ lib/dvb/decoder.cpp
@@ -77,7 +77,11 @@ int eDVBAudio::startPid(int pid, int type)
 			pes.pes_type = DMX_PES_AUDIO3;
 			break;
 		}
+#if defined(__sh__) // increases zapping speed
+		pes.flags    = DMX_IMMEDIATE_START;
+#else		
 		pes.flags    = 0;
+#endif		
 		eDebugNoNewLineStart("DMX_SET_PES_FILTER(0x%02x) - audio - ", pid);
 		if (::ioctl(m_fd_demux, DMX_SET_PES_FILTER, &pes) < 0)
 		{
@@ -92,6 +96,15 @@ int eDVBAudio::startPid(int pid, int type)
 			return -errno;
 		}
 		eDebugNoNewLineEnd("ok");
+#if not defined(__sh__) // already startet cause of DMX_IMMEDIATE_START
+		eDebugNoNewLineStart("[eDVBAudio%d] DEMUX_START ", m_dev);
+		if (::ioctl(m_fd_demux, DMX_START) < 0)
+		{
+			eDebugNoNewLine("failed: %m\n");
+			return -errno;
+		}
+		eDebugNoNewLine("ok\n");
+#endif		
 	}
 
 	if (m_fd >= 0)
@@ -139,7 +152,9 @@ int eDVBAudio::startPid(int pid, int type)
 			eDebugNoNewLineEnd("failed (%m)");
 		else
 			eDebugNoNewLineEnd("ok");
+#if not defined(__sh__) // this is a hack which only matters for dm drivers			
 		freeze();  // why freeze here?!? this is a problem when only a pid change is requested... because of the unfreeze logic in Decoder::setState
+#endif		
 		eDebugNoNewLineStart("AUDIO_PLAY - ");
 		if (::ioctl(m_fd, AUDIO_PLAY) < 0)
 			eDebugNoNewLineEnd("failed (%m)");
@@ -375,7 +390,11 @@ int eDVBVideo::startPid(int pid, int type)
 			pes.pes_type = DMX_PES_VIDEO3;
 			break;
 		}
+#if defined(__sh__) // increases zapping speed
+		pes.flags    = DMX_IMMEDIATE_START;
+#else		
 		pes.flags    = 0;
+#endif		
 		eDebugNoNewLineStart("DMX_SET_PES_FILTER(0x%02x) - video - ", pid);
 		if (::ioctl(m_fd_demux, DMX_SET_PES_FILTER, &pes) < 0)
 		{
@@ -383,6 +402,7 @@ int eDVBVideo::startPid(int pid, int type)
 			return -errno;
 		}
 		eDebugNoNewLineEnd("ok");
+#if not defined(__sh__) // already startet cause of DMX_IMMEDIATE_START		
 		eDebugNoNewLineStart("DEMUX_START - video - ");
 		if (::ioctl(m_fd_demux, DMX_START) < 0)
 		{
@@ -390,11 +410,14 @@ int eDVBVideo::startPid(int pid, int type)
 			return -errno;
 		}
 		eDebugNoNewLineEnd("ok");
+#endif		
 	}
 
 	if (m_fd >= 0)
 	{
+#if not defined(__sh__) // this is a hack which only matters for dm drivers		
 		freeze();  // why freeze here?!? this is a problem when only a pid change is requested... because of the unfreeze logic in Decoder::setState
+#endif		
 		eDebugNoNewLineStart("VIDEO_PLAY - ");
 		if (::ioctl(m_fd, VIDEO_PLAY) < 0)
 			eDebugNoNewLineEnd("failed (%m)");
@@ -707,10 +730,14 @@ int eDVBPCR::startPid(int pid)
 		pes.pes_type = DMX_PES_PCR2;
 		break;
 	case 3:
-		pes.pes_type = DMX_PES_PCR3;
+		pes.pes_type = DMX_PES_PCR3;		
 		break;
 	}
+#if defined(__sh__) // increases zapping speed
+	pes.flags    = DMX_IMMEDIATE_START;
+#else	
 	pes.flags    = 0;
+#endif	
 	eDebugNoNewLineStart("DMX_SET_PES_FILTER(0x%02x) - pcr - ", pid);
 	if (::ioctl(m_fd_demux, DMX_SET_PES_FILTER, &pes) < 0)
 	{
@@ -718,6 +745,7 @@ int eDVBPCR::startPid(int pid)
 		return -errno;
 	}
 	eDebugNoNewLineEnd("ok");
+#if not defined(__sh__) // already startet cause of DMX_IMMEDIATE_START	
 	eDebugNoNewLineStart("DEMUX_START - pcr - ");
 	if (::ioctl(m_fd_demux, DMX_START) < 0)
 	{
@@ -725,6 +753,7 @@ int eDVBPCR::startPid(int pid)
 		return -errno;
 	}
 	eDebugNoNewLineEnd("ok");
+#endif	
 	return 0;
 }
 
@@ -780,7 +809,11 @@ int eDVBTText::startPid(int pid)
 		pes.pes_type = DMX_PES_TELETEXT3;
 		break;
 	}
+#if defined(__sh__) // increases zapping speed
+	pes.flags    = DMX_IMMEDIATE_START;
+#else	
 	pes.flags    = 0;
+#endif	
 
 	eDebugNoNewLineStart("DMX_SET_PES_FILTER(0x%02x) - ttx - ", pid);
 	if (::ioctl(m_fd_demux, DMX_SET_PES_FILTER, &pes) < 0)
@@ -789,6 +822,7 @@ int eDVBTText::startPid(int pid)
 		return -errno;
 	}
 	eDebugNoNewLineEnd("ok");
+#if not defined(__sh__) // already startet cause of DMX_IMMEDIATE_START	
 	eDebugNoNewLineStart("DEMUX_START - ttx - ");
 	if (::ioctl(m_fd_demux, DMX_START) < 0)
 	{
@@ -796,6 +830,7 @@ int eDVBTText::startPid(int pid)
 		return -errno;
 	}
 	eDebugNoNewLineEnd("ok");
+#endif	
 	return 0;
 }
 
@@ -931,12 +966,28 @@ int eTSMPEGDecoder::setState()
 		int *s = state_table[m_state];
 		if (changed & (changeState|changeVideo) && m_video)
 		{
+#if not defined(__sh__) // see comment below			
 			m_video->setSlowMotion(s[1]);
 			m_video->setFastForward(s[2]);
+#endif			
 			if (s[0])
 				m_video->unfreeze();
 			else
 				m_video->freeze();
+#if defined(__sh__)
+// the VIDEO_CONTINUE would reset the FASTFORWARD  command so we
+// execute the FASTFORWARD after the VIDEO_CONTINUE
+			if (s[1])
+			{
+				m_video->setFastForward(s[2]);
+				m_video->setSlowMotion(s[1]);
+			}
+			else
+			{
+				m_video->setSlowMotion(s[1]);
+				m_video->setFastForward(s[2]);
+			}
+#endif				
 		}
 		if (changed & (changeState|changeAudio) && m_audio)
 		{
@@ -1237,6 +1288,10 @@ RESULT eTSMPEGDecoder::showSinglePic(const char *filename)
 		{
 			struct stat s;
 			fstat(f, &s);
+#if defined(__sh__) // our driver has a different behaviour for iframes
+                        if (m_video_clip_fd >= 0)
+                                finishShowSinglePic();
+#endif			
 			if (m_video_clip_fd == -1)
 				m_video_clip_fd = open("/dev/dvb/adapter0/video0", O_WRONLY);
 #if HAVE_HISILICON
@@ -1265,8 +1320,10 @@ RESULT eTSMPEGDecoder::showSinglePic(const char *filename)
 				if (ioctl(m_video_clip_fd, VIDEO_SELECT_SOURCE, VIDEO_SOURCE_MEMORY) < 0)
 					eDebug("VIDEO_SELECT_SOURCE MEMORY failed (%m)");
 #endif
+#if not defined(__sh__)
 				if (ioctl(m_video_clip_fd, VIDEO_SET_STREAMTYPE, streamtype) < 0)
 					eDebug("VIDEO_SET_STREAMTYPE failed(%m)");
+#endif					
 				if (ioctl(m_video_clip_fd, VIDEO_PLAY) < 0)
 					eDebug("VIDEO_PLAY failed (%m)");
 				if (ioctl(m_video_clip_fd, VIDEO_CONTINUE) < 0)
@@ -1283,10 +1340,12 @@ RESULT eTSMPEGDecoder::showSinglePic(const char *filename)
 				if (!seq_end_avail)
 					write(m_video_clip_fd, seq_end, sizeof(seq_end));
 				writeAll(m_video_clip_fd, stuffing, 8192);
+#if not defined(__sh__)				
 #if HAVE_HISILICON
 				;
 #else
 				m_showSinglePicTimer->start(150, true);
+#endif				
 #endif
 			}
 			close(f);
